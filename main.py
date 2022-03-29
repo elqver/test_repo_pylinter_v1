07bd39bcd39424517213732ce013626f40e8a013
Mikhail Novikov <elqver@gmail.com>
2022-03-30 01:43:20 +0300

    fixing as flake8 said

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import os
 import math
 
+
 def func1(a: int, b: int) -> str:
     return math.floor(a + b)
 
-def func2(a,b, c) -> int:
+
+def func2(a, b, c) -> int:
     return os.getcwd()
+
 
 def func3():
     func1(3.2, 3.5)
