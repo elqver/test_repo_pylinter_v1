90df5445df58e9604aa67549df2ee8442d1a017e
Mikhail Novikov <elqver@gmail.com>
2022-03-30 01:40:53 +0300

    add func3

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import os
 import math
 
-def func1(a, b) -> str:
+def func1(a: int, b: int) -> str:
     return math.floor(a + b)
 
 def func2(a,b, c) -> int:
     return os.getcwd()
+
+def func3():
+    func1(3.2, 3.5)
